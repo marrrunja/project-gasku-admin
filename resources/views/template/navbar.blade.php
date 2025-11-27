a10bfa9bd12924a62308d321acce9ce042ad2001
marrrunja <muammarirfan21@gmail.com>
2025-11-27 20:57:22 +0700

    udh jdi home

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,14 +1,31 @@
-<ul
-    class="nav bg-utama p-3 justify-content-around justify-content-md-evenly justify-content-lg-evenly sticky-top px-sm-0 px-lg-5 px-md-5">
-    <li class="nav-item awal">
-      <a class="nav-link brand text-white fw-bold home" href="home_adm.php">
-        <i class="bi bi-house-fill me-2"></i><span class="ilang">Home</span>
-      </a>
-    </li>
-    
-    <li class="nav-item tengah text-md-center">
-      <a class="nav-link text-center text-white fw-semibold brand " href="#" id="inputStokGas">
-        Input Stok Gas
-      </a>
-    </li>
-  </ul>
+<ul class="nav bg-utama p-3 justify-content-evenly sticky-top px-sm-0 px-lg-5 px-md-2">
+        <li class="nav-item tengah d-flex flex-column align-items-start position-relative">
+            <a class="nav-link text-white fw-bold brand m-0 p-0" href="#">
+                <img src="{{ asset('resources/images/gasPutih.png') }}" width="50" height="50" class="mb-1 img-brand"
+                    style="margin-right:-10px; transform: translateY(-3px);">
+                G<i class="bi bi-fire"></i>SKU
+            </a>
+            <!-- Tambahkan teks admin dengan posisi lebih ke kanan -->
+            <span class="text-white fw-semibold mt-1 posisi-admin">Admin</span>
+        </li>
+
+        <!-- Tambahan Dropdown -->
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white fw-bold" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                MENU
+            </a>
+            <ul class="dropdown-menu">
+                <li><button type="submit" class="dropdown-item text-success">
+                        <i class="bi bi-file-person me-2"></i><a href="#"
+                            class="text-decoration-none text-success">Buat Akun</a>
+                    </button>
+                </li>
+                <li><button type="submit" class="dropdown-item text-danger">
+                        <i class="bi bi-box-arrow-left me-2"></i><a href="../user/logout.php"
+                            class="text-decoration-none logout text-danger">Logout</a>
+                    </button>
+                </li>
+            </ul>
+        </li>
+    </ul>
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -7,8 +7,7 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
