b6dc1672a93c43f90209c8ab1cef2484984b8ff4
Griyo1009 <griyonugroho10@gmail.com>
2025-11-28 15:51:39 +0700

    bagian griyo

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/makeaccount', function () {
+    return view('admin.makeaccount');
+});
+Route::get('/admin/makeaccount/success', function () {
+    return view('admin.makeaccountsuccess');
+});
+Route::get('/admin/makeaccount/failed', function () {
+    return view('admin.makeaccountfailed');
+});
+Route::get('/admin/detail-akun', function () {
+    return view('admin.detail-akun');
+});
